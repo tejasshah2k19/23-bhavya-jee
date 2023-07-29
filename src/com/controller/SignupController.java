8b61fe919555af4c8550408ee5a7c5699842892a
tejas <tejasshah@gmail.com>
2023-07-29 20:35:12 +0530

    servlet

---
+++ src/com/controller/SignupController.java
@@ -0,0 +1,25 @@
+package com.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SignupController")
+public class SignupController extends HttpServlet{
+
+	
+	public void service(HttpServletRequest request,HttpServletResponse response) {
+		System.out.println("SignupController...");
+		
+		//read 
+		
+		//validation
+		
+		//db 
+		
+		//login 
+		
+	}
+	
+}
