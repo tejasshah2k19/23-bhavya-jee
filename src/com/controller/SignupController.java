aad31e0d174c523b85d47a8c6995c1f75c7d9c9f
tejas <tejasshah@gmail.com>
2023-07-31 16:11:54 +0530

    sc

--- src/com/controller/SignupController.java
+++ src/com/controller/SignupController.java
@@ -1,5 +1,8 @@
 package com.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,16 +12,50 @@ import javax.servlet.http.HttpServletResponse;
 public class SignupController extends HttpServlet{
 
 	
-	public void service(HttpServletRequest request,HttpServletResponse response) {
+	public void service(HttpServletRequest request,HttpServletResponse response) throws IOException{
 		System.out.println("SignupController...");
 		
 		//read 
+		String firstName = request.getParameter("firstName");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
 		
 		//validation
+		boolean isError = false; 
+		String error = "";
+
+		if(firstName == null || firstName.trim().length() == 0 ) {
+			isError = true;
+			error = error + "<br>Please Enter FirstName"; 
+		}
+		if(email == null || email.trim().length() == 0 ) {
+			isError = true; 
+			error = error + "<BR>Please Enter Email";
+		}
 		
-		//db 
+		if(password == null || password.trim().length() ==  0) {
+			isError= true;
+			error = error + "<br>Please Enter Password";
+		}
 		
-		//login 
+		PrintWriter out = response.getWriter(); //out -> data -> browser  
+		response.setContentType("text/html");//MIME  
+		
+		out.print("<html><body>");
+		if(isError == true) {
+			//error 
+			out.print(error);
+		}else {
+			//success
+			out.print("FirstName : "+firstName);
+			out.print("<br>Email : "+email);
+			out.print("<br>Password : "+password);
+			
+		}
+		
+		out.print("</body></html>");
+		
+		 
 		
 	}
 	
